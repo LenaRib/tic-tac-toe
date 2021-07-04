9f93c41039596ca8e14aa024056c5732d1d901da
LenaRib <poddubnayaeo@gmail.com>
2021-07-04 20:12:58 +0200

    feat: add getWinner

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -2,9 +2,9 @@ class TicTacToe {
     constructor() {
         this.currentPlayer = 'x';
         this.turns = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
         ];
         this.winner = '';
     }
@@ -14,7 +14,7 @@ class TicTacToe {
     }
 
     nextTurn(rowIndex, columnIndex) {
-        if (this.turns[rowIndex][columnIndex] === 0) {
+        if (this.turns[rowIndex][columnIndex] === null) {
             this.turns[rowIndex][columnIndex] = this.currentPlayer;
             this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
         }
@@ -22,9 +22,30 @@ class TicTacToe {
 
     isFinished() {
 
+        this.winner = '';
+        return true || false;
     }
 
     getWinner() {
+        for (let i = 0; i < 2; i += 1) {
+            if (this.turns[i].every(e => e === 'x')) this.winner = 'x';
+            if (this.turns[i].every(e => e === 'y')) this.winner = 'y';
+        }
+
+        for (let i = 0; i < 2; i += 1) {
+            let winner = turns[i][0];
+            let winnerCount = 1;
+            for (let j = 1; j < 2; j += 1) {
+
+                if (this.turns[i][j] === winner)
+                    winnerCount += 1;
+            }
+            if (winnerCount === 3) this.winner = winner;
+        }
+
+        if (this.turns[0][0] === this.turns[1][1] === this.turns[2][2]) this.winner = turns[1][1];
+        if (this.turns[0][2] === this.turns[1][1] === this.turns[2][0]) this.winner = turns[1][1];
+
         return this.winner;
     }
 
